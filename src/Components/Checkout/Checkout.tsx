ec2c9811994dc1783e5a4e8c8b5ebbe10554fbd4
tsuri <tsuri.he@gmail.com>
2024-04-07 21:58:25 +0300

    f

--- src/Components/Checkout/Checkout.tsx
+++ src/Components/Checkout/Checkout.tsx
@@ -3,7 +3,22 @@ import "./Checkout.css";
 function Checkout(): JSX.Element {
     return (
         <div className="Checkout">
-			
+            <form>
+                <div className="billing-details">
+                    <label htmlFor="name"></label>
+                    <input id="name" />
+
+                    <label htmlFor="name"></label>
+                    <input id="name" />
+
+                    <label htmlFor="name"></label>
+                    <input id="name" />
+                </div>
+                <div className="shipping-info"></div>
+                <div className="payment-details"></div>
+            </form>
+
+
         </div>
     );
 }
