9da16d02bae0862579ca454c52f4681030df0ed2
tsuri <tsuri.he@gmail.com>
2024-04-14 23:05:01 +0300

    dfgfdg

--- src/Components/Layout/Layout/Layout.tsx
+++ src/Components/Layout/Layout/Layout.tsx
@@ -24,7 +24,7 @@ export function Layout(): JSX.Element {
             </header>
 
             <main>
-                {/* <Main /> */}
+                <Main />
                 {/* <HeadphonesSection /> */}
                 {/* <SpeakersSection /> */}
                 {/* <EarphoneSection /> */}
@@ -35,7 +35,7 @@ export function Layout(): JSX.Element {
                 {/* <Zx7 /> */}
                 {/* <Yx1 /> */}
                 {/* <Checkout /> */}
-                <ThankYouPage />
+                {/* <ThankYouPage /> */}
             </main>
 
             <footer>
